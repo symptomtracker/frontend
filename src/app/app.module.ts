edfbc4949a1c9e76a7686b60d141c9617c12542a
Jana Esselborn <janaesselborn@gmail.com>
2020-03-21 17:15:19 +0100

    Add routing between the pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,50 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageViewComponent } from './landing-page-view/landing-page-view.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {AlertModule} from 'ngx-bootstrap';
+import { AlertModule } from 'ngx-bootstrap';
 import { ProfileBasicViewComponent } from './profile-basic-view/profile-basic-view.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { SymptomBasicViewComponent } from './symptom-basic-view/symptom-basic-view.component';
+import { RouterModule, Routes } from "@angular/router";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProfileContactViewComponent } from "./profile-contact-view/profile-contact-view.component";
+
+const appRoutes: Routes = [
+  { path: 'home', component: LandingPageViewComponent },
+  { path: 'profilebasic', component: ProfileBasicViewComponent },
+  { path: 'profilecontact', component: ProfileContactViewComponent },
+  { path: 'symptombasic', component: SymptomBasicViewComponent },
+/*  {
+    path: 'heroes',
+    component: HeroListComponent,
+    data: { title: 'Heroes List' }
+  }, */
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageViewComponent,
     ProfileBasicViewComponent,
+    ProfileContactViewComponent,
     ToolbarComponent,
     SymptomBasicViewComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AlertModule.forRoot()
+    AlertModule.forRoot(),
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
