304bcaf4d50ff3d2d4825fa200a63dd44b1fc908
Jana Esselborn <janaesselborn@gmail.com>
2020-03-22 12:47:34 +0100

    Add patient overview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,11 @@ import { RouterModule, Routes } from "@angular/router";
 import { ProfileContactViewComponent } from "./profile-contact-view/profile-contact-view.component";
 import { PrivacyStatementViewComponent } from './privacy-statement-view/privacy-statement-view.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { CodeInputViewComponent } from './code-input-view/code-input-view.component';
 import { DashboardViewComponent } from './dashboard-view/dashboard-view.component';
 import { MyDataViewComponent } from './my-data-view/my-data-view.component';
+import { HealthOfficeViewComponent } from './health-office-view/health-office-view.component';
 
 const appRoutes: Routes = [
   { path: 'home',
@@ -29,6 +30,7 @@ const appRoutes: Routes = [
   { path: 'impressum', component: LegalNoticeComponent },
   { path: 'datenschutzerklärung', component: PrivacyStatementViewComponent },
   { path: 'meinedaten', component: MyDataViewComponent },
+  { path: 'overviewpatients', component: HealthOfficeViewComponent },
   { path: '',
     redirectTo: '/home',
     pathMatch: 'full'
@@ -50,6 +52,7 @@ const appRoutes: Routes = [
     CodeInputViewComponent,
     DashboardViewComponent,
     MyDataViewComponent,
+    HealthOfficeViewComponent,
   ],
   imports: [
     BrowserModule,
@@ -61,6 +64,9 @@ const appRoutes: Routes = [
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
     )
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
