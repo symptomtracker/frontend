3b716e4f987793ff7081a5752919a8ee2834a5d0
Maximilian von Unwerth <maximilian@von-unwerth.de>
2020-03-27 18:59:47 +0100

    spec.ts Router Module

--- src/app/profile-basic-view/profile-basic-view.component.spec.ts
+++ src/app/profile-basic-view/profile-basic-view.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfileBasicViewComponent } from './profile-basic-view.component';
+import {RouterModule} from "@angular/router";
 
 describe('ProfileBasicViewComponent', () => {
   let component: ProfileBasicViewComponent;
