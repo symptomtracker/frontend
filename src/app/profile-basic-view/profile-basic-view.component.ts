ee8888a6862eb823265f4829c1ceae78f581b921
Maximilian von Unwerth <maximilian@von-unwerth.de>
2020-03-27 18:43:14 +0100

    Routing in profile-basic-view , Name, Age are required

--- src/app/profile-basic-view/profile-basic-view.component.ts
+++ src/app/profile-basic-view/profile-basic-view.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { PatientService } from '../_service/patient.service';
-import { PatientModel } from '../_service/api';
+import {Component, OnInit} from '@angular/core';
+import {PatientService} from '../_service/patient.service';
+import {PatientModel} from '../_service/api';
+import {Router, RouterModule, Routes} from "@angular/router";
 
 @Component({
   selector: 'app-profile-basic-view',
@@ -9,22 +10,25 @@ import { PatientModel } from '../_service/api';
 })
 export class ProfileBasicViewComponent implements OnInit {
 
-  constructor(private patientService:PatientService) { }
+  constructor(private patientService: PatientService, private router: Router) {
+  }
 
   ngOnInit(): void {
     this.loadPatient();
   }
-  elements ={
-    gender:"male",
-    joblabel:null,
-    contact:null
+
+  elements = {
+    gender: "male",
+    joblabel: null,
+    contact: null
   }
 
-  gender:string;
+  gender: string;
   patient: PatientModel = {}; //TODO Empty object
-  joblabel:number;
-  async loadPatient(){
-    this.patient = (await this.patientService.getPatient("123")).data
+  joblabel: number;
+
+  async loadPatient() {
+    this.patient = (await this.patientService.getPatient("123")).data;
     // this.gender ="male";
     console.log(this.patient);
 
@@ -39,11 +43,30 @@ export class ProfileBasicViewComponent implements OnInit {
     targetLabel.classList.add("active");
   }
 
-  onSubmit(){
-    switch(this.elements.contact){
-      case "nocontact":
-        this.patient.relatedAttributes.push({Type:"contact", Value:"nocontact"})
-        break;
+  onSubmit() {
+    let allAreFilled = true;
+    let name = (<HTMLInputElement>document.getElementById("nameInput")).value;
+    if (name.length == 0) {
+      allAreFilled = false;
+    } //TODO eigenen Fehler für no name und andere Felder
+
+    let ageField = (<HTMLInputElement>document.getElementById("ageInput"));
+    if (!ageField || !ageField.value) {
+      allAreFilled = false;
+    }
+
+    //TODO Optionfields, ...
+
+    if (allAreFilled) {
+      const url = 'profilecontact';
+      this.router.navigateByUrl(url);
+      switch (this.elements.contact) {
+        case "nocontact":
+          this.patient.relatedAttributes.push({Type: "contact", Value: "nocontact"})
+          break;
+      }
+    } else {
+      alert("Nicht alle Felder ausgefüllt.")
     }
 
   }
